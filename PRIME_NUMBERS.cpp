60625ffa1aa61140851e75ba689ee04b472b583d
Maru Dhanvi <marudhanvi8@gmail.com>
2025-06-15 17:28:03 +0530

    Update and rename PRIME.CPP to PRIME_NUMBERS.cpp

--- PRIME_NUMBERS.cpp
+++ PRIME_NUMBERS.cpp
@@ -1,5 +1,8 @@
-#incude<iostream.h>
-#include<conio.h>
+#incude<iostream>
+using namespace std;
+
+//PRIME NUMBERS
+
 int main()
 {
  int n,i,m=0,flag=0;
@@ -7,17 +10,18 @@ int main()
  cout <<"enter number"<<endl;
  cin>> n;
  m=n/2;
+ 
  for (i=2;i<=m;i++){
  if(n%i==0){
  flag=1;
  break;
  }
  }
- if(flag==0  ){
+ 
+ if(flag==0){
  cout<<"prime no"<<endl;
  }else{
- cout <<"no prime"<<endl;
+ cout <<"not prime"<<endl;
  }
- getch();
  return 0;
  }
